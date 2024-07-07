c5711dc156767efafeba243eeb015ffa3042ef31
Madeshwaran.S <166493024+madeshwaranbtech@users.noreply.github.com>
2024-07-07 10:38:32 +0530

    Time: 4 ms (99.94%) | Memory: 56.8 MB (23.64%) - LeetSync

--- 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.java
+++ 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.java
@@ -1,27 +1,18 @@
-
 class Solution {
     public int minKBitFlips(int[] nums, int k) {
-          Queue<Integer> q=new LinkedList<>();
-	      
-          int count=0;
-	      
-          for(int i=0;i<nums.length;i++) {
-	    	  int curflips=q.size();
-	    	  int conv=-1;
-	    	  if(curflips%2==0) 
-                conv=1;
-	    	  else 
-                conv=0;
-	    	  
-              if(nums[i]!=conv) {
-	    		  q.add(i+k-1);
-	    		  count++;
-	    	  }
-	    	  if(!q.isEmpty()&&q.peek()<=i)
-	    		  q.poll();
-	      }
-	      if(!q.isEmpty()) 
-            return -1;
-	      return count;
+        int n = nums.length, flipped = 0, res = 0;
+        int[] isFlipped = new int[n];
+        for (int i = 0; i < nums.length; ++i) {
+            if (i >= k)
+                flipped ^= isFlipped[i - k];
+                if (flipped == nums[i]) {
+                    if (i + k > nums.length)
+                        return -1;
+                isFlipped[i] = 1;
+                flipped ^= 1;
+                res++;
+            }
+        }
+        return res;
     }
 }
