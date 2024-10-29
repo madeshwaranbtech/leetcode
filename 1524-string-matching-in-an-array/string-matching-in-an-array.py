abd50c051bc388bd9f86f05cd32b81615591104f
Madeshwaran.S <166493024+madeshwaranbtech@users.noreply.github.com>
2024-10-29 10:00:53 +0530

    Time: 5 ms (35.90%) | Memory: 11.7 MB (50.20%) - LeetSync

---
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def stringMatching(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        res=[]
+        for i in range(len(words)):
+            for j in range(len(words)):
+                if i!=j and words[i] in words[j]:
+                    res.append(words[i])
+                    break
+        return res
